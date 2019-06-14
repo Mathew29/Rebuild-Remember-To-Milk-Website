350cf92c99004a45c4e6f0cd2e0cddbcf828a9f2
Student <example@example.com>
2019-06-14 08:53:54 -0700

    add a Description class that can be exported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
