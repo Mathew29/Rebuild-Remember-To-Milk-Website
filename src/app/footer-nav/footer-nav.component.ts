c84ec0e48d1eb20e27809d0dd8734b13584c6448
Student <example@example.com>
2019-06-14 10:29:14 -0700

    add @Input, @Output and import all footer arrays

--- src/app/footer-nav/footer-nav.component.ts
+++ src/app/footer-nav/footer-nav.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FooterCompany } from '../models/footer-company.models';
+import { FooterProduct } from '../models/footer-product.models';
+import { FooterCommunity } from '../models/footer-community.models';
+import { FooterSupport } from '../models/footer-support.models';
 
 @Component({
   selector: 'app-footer-nav',
   templateUrl: './footer-nav.component.html',
   styleUrls: ['./footer-nav.component.css']
 })
-export class FooterNavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class FooterNavComponent {
+@Input() childFooterCompany: FooterCompany[];
+@Input() childFooterProduct: FooterProduct[];
+@Input() childFooterCommunity: FooterCommunity[];
+@Input() childFooterSupport: FooterSupport[];
+@Output() clickFooter = new EventEmitter();
 
 }
