c028171469f9e4cfddc5a93d1b46d659eb2995d8
Mathew Akre <mwakre29@gmail.com>
2019-06-21 10:08:12 -0700

    add firebase routing and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 import { routing } from './app.routing';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { masterFirebaseConfig } from './api-keys';
 // import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { FooterNavComponent } from './footer-nav/footer-nav.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
 import { TourComponent } from './tour/tour.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
+
 
 @NgModule({
   declarations: [
@@ -23,7 +34,9 @@ import { TourComponent } from './tour/tour.component';
     AppRoutingModule,
     FormsModule,
     // HttpModule,
-    routing
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
