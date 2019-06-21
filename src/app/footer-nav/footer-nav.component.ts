f870b991e8d5dc28bba4b539dd524dfb0a13fd54
Mathew Akre <mwakre29@gmail.com>
2019-06-21 08:47:06 -0700

    remoce footer arrays

--- src/app/footer-nav/footer-nav.component.ts
+++ src/app/footer-nav/footer-nav.component.ts
@@ -1,8 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FooterCompany } from '../models/footer-company.models';
-import { FooterProduct } from '../models/footer-product.models';
-import { FooterCommunity } from '../models/footer-community.models';
-import { FooterSupport } from '../models/footer-support.models';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-footer-nav',
@@ -10,10 +7,6 @@ import { FooterSupport } from '../models/footer-support.models';
   styleUrls: ['./footer-nav.component.css']
 })
 export class FooterNavComponent {
-@Input() childFooterCompany: FooterCompany[];
-@Input() childFooterProduct: FooterProduct[];
-@Input() childFooterCommunity: FooterCommunity[];
-@Input() childFooterSupport: FooterSupport[];
-@Output() clickFooter = new EventEmitter();
+
 
 }
