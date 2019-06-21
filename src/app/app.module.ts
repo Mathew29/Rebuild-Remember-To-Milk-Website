941db062ac2567c6f993299ee97378c2a58ce0d2
Mathew Akre <mwakre29@gmail.com>
2019-06-21 09:03:54 -0700

    imports routing and Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { routing } from './app.routing';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+// import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { FooterNavComponent } from './footer-nav/footer-nav.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { TourComponent } from './tour/tour.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterNavComponent,
     MainHeaderComponent,
-    HeaderNavComponent
+    HeaderNavComponent,
+    TourComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    // HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
