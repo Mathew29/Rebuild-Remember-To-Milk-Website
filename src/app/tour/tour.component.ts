cc7166dca14d8d955e7ceb0606fa999a821e7783
Mathew Akre <mwakre29@gmail.com>
2019-06-21 11:29:44 -0700

    firebase subscribe

--- src/app/tour/tour.component.ts
+++ src/app/tour/tour.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { TourService } from '../tour.service';
 
 @Component({
   selector: 'app-tour',
   templateUrl: './tour.component.html',
-  styleUrls: ['./tour.component.css']
+  styleUrls: ['./tour.component.css'],
+  providers: [TourService]
 })
 export class TourComponent implements OnInit {
+  employees: FirebaseListObservable<any[]>;
+  constructor(private service: TourService) { 
+    this.employees = service.getEmployees();
 
-  constructor() { }
+    this.employees.subscribe( items => { 
+      console.log(items);
+      
+    });
+  }
 
   ngOnInit() {
   }
