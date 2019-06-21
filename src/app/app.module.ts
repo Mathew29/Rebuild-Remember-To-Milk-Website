f047bf7fa3620c82a5fc2337366c8e9bbc797304
Mathew Akre <mwakre29@gmail.com>
2019-06-21 13:07:21 -0700

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,11 @@ import { FooterNavComponent } from './footer-nav/footer-nav.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
 import { TourComponent } from './tour/tour.component';
+import { NewComponent } from './new/new.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+import { HelpComponent } from './help/help.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -27,7 +32,12 @@ export const firebaseConfig = {
     FooterNavComponent,
     MainHeaderComponent,
     HeaderNavComponent,
-    TourComponent
+    TourComponent,
+    NewComponent,
+    UpgradeComponent,
+    HelpComponent,
+    LogInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
