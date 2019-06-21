5c0baa97365e9565ca9b65058f3c44905635fd8d
Mathew Akre <mwakre29@gmail.com>
2019-06-21 13:07:53 -0700

    add routes for the top nav bar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,12 @@
-import { ModuleWithProviders } from '@angular/core';
+import { ModuleWithProviders, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { TourComponent } from './tour/tour.component';
+import { NewComponent } from './new/new.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+import { HelpComponent } from './help/help.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const appRoutes: Routes = [
     {
@@ -11,6 +16,26 @@ const appRoutes: Routes = [
     {
         path: 'tour',
         component: TourComponent
+    },
+    {
+        path: 'new',
+        component: NewComponent
+    },
+    {
+        path: 'upgrade',
+        component: UpgradeComponent
+    },
+    {
+        path: 'help',
+        component: HelpComponent
+    },
+    {
+        path: 'log-in',
+        component: LogInComponent
+    },
+    {
+        path: 'sign-up',
+        component: SignUpComponent
     }
 ];
 
